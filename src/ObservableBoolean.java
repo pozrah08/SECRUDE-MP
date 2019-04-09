0332ee52738f8789906ab90e98e005c1cce72789
KADO\Dante <cristina_aquino@dlsu.edu.ph>
2019-04-09 17:24:22 +0800

    new class (needed for implementing debugging) ObservableBoolean.java

---
+++ src/ObservableBoolean.java
@@ -0,0 +1,42 @@
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dante
+ */
+public class ObservableBoolean {
+
+    // "CopyOnWrite" to avoid concurrent modification exceptions in loop below.
+    private final List<ChangeListener> listeners =
+            new CopyOnWriteArrayList<ChangeListener>();
+
+    private boolean value;
+
+    public boolean getValue() {
+        return value;
+    }
+
+    public synchronized void setValue(boolean b) {
+        value = b;
+        for (ChangeListener cl : listeners)
+            cl.stateChanged(new ChangeEvent(this));
+    }
+
+    public synchronized void addChangeListener(ChangeListener cl) {
+        listeners.add(cl);
+    }
+
+    public synchronized void removeChangeListener(ChangeListener cl) {
+        listeners.remove(cl);
+    }
+}    
